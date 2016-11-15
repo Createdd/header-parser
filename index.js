81646ee9bda6337a8d2f166e633bed00ea6b7542
Daniel Deutsch <deudan1010gmail.com>
2016-11-15 16:48:15 +0100

    finalising

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 "use strict";
-var express=require("express");
-var app=express();
-var port=process.env.PORT || 3000;
-var routes=require("./routes/index.js");
+var express=require("express");//add the express module to serve static files
+var app=express();//calling the express function
+var port=process.env.PORT || 3000;//set the port to arguments or default 3000
 
-app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
-routes(app);
+app.route("/whoami").get(function(req,res){
+      var data = {
+        "IP-Address":req.headers["host"],//displaying the ip address
+        "Language": req.headers["accept-language"].split(",")[0],//displaying only the first array element
+        "Operating System": req.headers["user-agent"].split(')')[0].split('(')[1]//displaying only the operating system
+      };//store the header data as an object. headers are stored in a JS object with the header key as strings
+      res.send(data);//send the data object
+    });//set a route to "whoami" and get header data
 
 app.listen(port, function(){
   console.log("Listenening on port: "+port);
-});
+});//set the listening port
