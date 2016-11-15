83c9e6da4365a27e3a8bde13de9658b0bf6f03f8
Daniel Deutsch <deudan1010gmail.com>
2016-11-15 17:09:44 +0100

    finalising

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", function(req,res){
   res.send("Add /whoami");
 })
 
-
+/*
 app.route("/whoami").get(function(req,res){
       var data = {
         "IP-Address":req.headers["host"],//displaying the ip address
@@ -16,8 +16,8 @@ app.route("/whoami").get(function(req,res){
         "Operating System": req.headers["user-agent"].split(')')[0].split('(')[1]//displaying only the operating system
       };//store the header data as an object. headers are stored in a JS object with the header key as strings
       res.send(data);//send the data object
-    });//set a route to "whoami" and get header data
+    });//set a route to "whoami" and get header data*/
 
 app.listen(port, function(){
-  console.log("Listenening on port: "+port);
+  console.log("Listening on port: "+port);
 });//set the listening port
